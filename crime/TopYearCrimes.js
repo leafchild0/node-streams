f068c1ffefddf589595a1ba520cf2f7693dbd36a
Viktor Malyshev <vmalyshev0@gmail.com>
2023-09-14 14:50:53 +0200

    fix bug in years, index was wrong

--- crime/TopYearCrimes.js
+++ crime/TopYearCrimes.js
@@ -22,12 +22,12 @@ export class TopYearCrimes extends Transform {
 
     _flush(callback) {
         // Sort it out, then check whether last year amount is <=> of the last, stream one word with result
-        const sorted = new Map([...this.crimesPerYear.entries()].sort())
+        const sorted = new Map([...this.crimesPerYear.entries()].sort((v1, v2) => v1[1] - v2[1]))
         let result = 'same';
         let keys = Array.from(sorted.keys());
 
-        if (sorted.get(keys[0]) > sorted.get(keys[keys.length])) result = 'up'
-        else if (sorted.get(keys[0]) < sorted.get(keys[keys.length])) result = 'down'
+        if (sorted.get(keys[0]) > sorted.get(keys[keys.length - 1])) result = 'up'
+        else if (sorted.get(keys[0]) < sorted.get(keys[keys.length - 1])) result = 'down'
 
         this.push(result, 'utf8')
         callback()
